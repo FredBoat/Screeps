d24eb347cfe4da10c8f1f5d296a081d6d6787e4e
Frederikam <frogkr@gmail.com>
2017-09-03 13:24:45 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-screeps');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    var credentials = grunt.file.readJSON('credentials.json');
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: credentials.email,
+                password: credentials.password,
+                branch: 'v2.0',
+                ptr: false
+            },
+            dist: {
+                src: ['dist/*.js']
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['src/**/*.js'],
+                tasks: ['clean','copy','screeps']
+            },
+        },
+
+        copy: {
+            screeps: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: '**',
+                    dest: 'dist/',
+                    filter: 'isFile',
+                    rename: function (dest, src) {
+                        // Change the path name utilize dots for folders
+                        return dest + src.replace(/\//g,'.');
+                    }
+                }]
+            },
+        },
+
+        clean: {
+            'dist': ['dist']
+        }
+    });
+
+    grunt.registerTask('default', ['clean','copy','screeps']);
+}
